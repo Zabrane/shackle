0561b84329bdeb2159f9a75ad69ea85af8b6d0d2
Zabrane <zabrane3@gmail.com>
2020-12-13 23:19:38 +0100

    Update shackle_defaults.hrl

--- include/shackle_defaults.hrl
+++ include/shackle_defaults.hrl
@@ -5,7 +5,7 @@
 -define(DEFAULT_CONNECT_TIMEOUT, 500).
 -define(DEFAULT_MAX_RETRIES, 2).
 -define(DEFAULT_POOL_SIZE, 16).
--define(DEFAULT_POOL_STRATEGY, round_robin).
+-define(DEFAULT_POOL_STRATEGY, random).
 -define(DEFAULT_PROTOCOL, shackle_tcp).
 -define(DEFAULT_RECONNECT, true).
 -define(DEFAULT_RECONNECT_MAX, timer:minutes(2)).
